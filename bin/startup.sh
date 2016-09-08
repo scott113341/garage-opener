1009830ff32d79f19153043cd12dfc702ee29f51
Scott Hardy <scott.the.hardy@gmail.com>
2016-09-07 20:38:02 -0700

    asdf

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #/etc/init.d/garage-opener.sh
 
-/root/.nvm/versions/node/v6.4.0/bin/node /root/code/garage-opener/lib/cli.js 0
+/root/.nvm/versions/node/v6.4.0/bin/node /root/code/garage-opener/lib/cli.js 0 1
